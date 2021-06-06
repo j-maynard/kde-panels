892a3958693c99dfb30cff64524142c2e7b82be8
jamiemaynard <jamie.maynard@digital.cabinet-office.gov.uk>
2021-06-06 11:36:08 +0100

    Added a gnome style panel
    This is based off my HUD Panel but the clock in the middle and expands
    the panel to the full width of the screen.

---
+++ panels/org.kde.plasma.desktop.gnomePanel/contents/layout.js
@@ -0,0 +1,103 @@
+var panel = new Panel
+var panelScreen = panel.screen
+var freeEdges = {"bottom": false, "top": true, "left": false, "right": false}
+
+for (i = 0; i < panelIds.length; ++i) {
+    var tmpPanel = panelById(panelIds[i])
+    if (tmpPanel.screen == panelScreen) {
+        // Ignore the new panel
+        if (tmpPanel.id != panel.id) {
+            freeEdges[tmpPanel.location] = false;
+        }
+    }
+}
+
+if (freeEdges["top"] == true) {
+    panel.location = "top";
+} else {
+    // There is no free edge, so leave the default value
+    panel.location = "top";
+}
+// For an Icons-Only Task Manager on the bottom, *3 is too much, *2 is too little
+// Round down to next highest even number since the Panel size widget only displays
+// even numbers
+panel.height = 48
+
+// Restrict horizontal panel to a maximum size of a 21:9 monitor
+const maximumAspectRatio = 21/9;
+if (panel.formFactor === "horizontal") {
+    const geo = screenGeometry(panelScreen);
+    const maximumWidth = Math.ceil(geo.height * maximumAspectRatio);
+
+    if (geo.width > maximumWidth) {
+        panel.alignment = "center";
+        panel.minimumLength = maximumWidth;
+        panel.maximumLength = maximumWidth;
+    }
+}
+panel.visibility = "windowsGoBelow"
+
+var kicker = panel.addWidget("org.kde.plasma.kicker")
+kicker.writeConfig("global", "Alt+F1")
+kicker.currentConfigGroup = ["Shortcuts"]
+kicker.writeConfig("alphaSort", "true")
+kicker.writeConfig("favoritesPortedToKAstats", "true")
+kicker.writeConfig("showIconsRootLevel", "true")
+panel.addWidget("org.kde.plasma.showdesktop")
+panel.addWidget("com.github.zren.presentwindows")
+var windowTitle = panel.addWidget("org.kde.windowtitle")
+windowTitle.writeConfig("showIcon", "false")
+windowTitle.writeConfig("style", "Application")
+panel.addWidget("org.kde.plasma.panelspacer")
+var clock = panel.addWidget("org.kde.plasma.digitalclock")
+clock.writeConfig("showDate", "false")
+panel.addWidget("org.kde.plasma.panelspacer")
+panel.addWidget("org.kde.plasma.virtualdesktopbar")
+
+/* Next up is determining whether to add the Input Method Panel
+ * widget to the panel or not. This is done based on whether
+ * the system locale's language id is a member of the following
+ * white list of languages which are known to pull in one of
+ * our supported IME backends when chosen during installation
+ * of common distributions. */
+
+var langIds = ["as",    // Assamese
+               "bn",    // Bengali
+               "bo",    // Tibetan
+               "brx",   // Bodo
+               "doi",   // Dogri
+               "gu",    // Gujarati
+               "hi",    // Hindi
+               "ja",    // Japanese
+               "kn",    // Kannada
+               "ko",    // Korean
+               "kok",   // Konkani
+               "ks",    // Kashmiri
+               "lep",   // Lepcha
+               "mai",   // Maithili
+               "ml",    // Malayalam
+               "mni",   // Manipuri
+               "mr",    // Marathi
+               "ne",    // Nepali
+               "or",    // Odia
+               "pa",    // Punjabi
+               "sa",    // Sanskrit
+               "sat",   // Santali
+               "sd",    // Sindhi
+               "si",    // Sinhala
+               "ta",    // Tamil
+               "te",    // Telugu
+               "th",    // Thai
+               "ur",    // Urdu
+               "vi",    // Vietnamese
+               "zh_CN", // Simplified Chinese
+               "zh_TW"] // Traditional Chinese
+
+if (langIds.indexOf(languageId) != -1) {
+    panel.addWidget("org.kde.plasma.kimpanel");
+}
+panel.addWidget("fokus")
+panel.addWidget("org.kde.plasma.systemtray")
+var userSwitcher = panel.addWidget("org.kde.plasma.userswitcher")
+userSwitcher.writeConfig("showFace", "true")
+userSwitcher.writeConfig("showName", "false")
